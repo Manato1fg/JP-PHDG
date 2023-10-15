41a7c400dfa4998ec76379a53756b6360c086dd9
Manato <manato-y@g.ecc.u-tokyo.ac.jp>
2023-10-15 17:52:29 +0900

    Support Index count

--- static/js/index.js
+++ static/js/index.js
@@ -13,12 +13,15 @@ document.addEventListener("DOMContentLoaded", async function() {
     const clearButton = document.querySelector("#canvas-control-clear")
     clearButton.addEventListener("click", () => clearDraw())
 
+    let index_count = 0
+
     async function getNextCharacter() {
-        const response = await fetch("/get-character")
+        const response = await fetch("/get-character?index_count=${index_count}")
         const data = await response.json()
         const character = data["character"]
         const c = document.querySelector("#character")
         c.textContent = character
+        index_count++
     }
 
     const nextButton = document.querySelector("#control-next")
